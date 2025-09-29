7c101352f380b858e8c896e0e18d3d229a4f9278
amiramtur <amiramtur22@gmail.com>
2025-09-29 14:27:00 +0300

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ScrollBase
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ScrollBase")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ScrollBase.Pages")]
